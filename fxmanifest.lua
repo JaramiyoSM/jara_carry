e7867aa66d303e9618982956905ae4baf02a9253
Jaramiiyo <jaramiyoservices@gmail.com>
2025-11-01 17:02:25 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,5 +5,10 @@ author 'jaramiyo'
 description 'Carry Other People / jaraservices.com'
 version '1.0.0'
 
+shared_scripts {
+    '@ox_lib/init.lua',
+}
+
 client_script "cl_carry.lua"
+
 server_script "sv_carry.lua"
